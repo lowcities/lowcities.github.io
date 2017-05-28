f2c37970c8e7b9a7c7b31e6acc0dc424f019eff0
David Low <musumashae@gmail.com>
2017-05-27 21:00:12 -0400

    added headlines to HTML and mave varoius formatting changes. Added a JS scroll feature that shows content dynamically as the page scrolls down.

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@ const menuButton = document.querySelector('.nav');
 const toggleMenu = document.querySelector('.nav_menu');
 const menu = document.querySelector('#menu');
 const content = document.getElementById('content');
-
-
-  
+const containers = document.querySelectorAll('.container');
+const link = document.getElementById('menu a');
+const topOfNav = menu.offsetTop;
+const headers = document.querySelectorAll('.headline');
 //get
 
 //
@@ -19,12 +20,6 @@ const checkSize = () => {
     menu.style.display = 'flex';
     }
   }
-
-//function check if an element contains a certain class
-function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-}
-  
 
 //AJAX to load links to page
 //  $('#menu a, #heading').on('click', function(e) {
@@ -47,20 +42,13 @@ toggleMenu.addEventListener('click', () => {
       menuButton.innerHTML = 'MENU'
       }
 });
-
-
-
-
 //Check size of screen on page load
 checkSize();
-
-
-
 //Check size of screen on resize of window
 $(window).resize(checkSize);
 
-const topOfNav = menu.offsetTop;
 
+//creates a sticky nav bar when window is scrolled to the top of nav bar.
 window.onscroll = (function(e) {
   let stuck = false;
   if(window.scrollY >= topOfNav && !stuck) {
@@ -73,7 +61,56 @@ window.onscroll = (function(e) {
   }
 });
 
+  function debounce(func, wait = 20, immediate = true) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
 
+//Show or hide content based on window position
+function showContent(e) {
+  containers.forEach(container => {
+    //half way through container
+    const showContainerAt = (window.scrollY + window.innerHeight) - container.offsetHeight / 8;
+    //botom of the container
+    const containerBottom = container.offsetTop + container.offsetHeight;
+    const isHalfShown = showContainerAt > container.offsetTop;
+    const isNotScrolledPast = window.scrollY < containerBottom;
+    if(isHalfShown && isNotScrolledPast) {
+      container.classList.add('container-show');
+    } else {
+      container.classList.remove('container-show');
+
+    }
+  });
+}
+
+function showHeadline(e) {
+  headers.forEach(headline => {
+    const showHeadlineAt = (window.scrollY + window.innerHeight) - headline.offsetHeight;
+    const headlineBottom = headline.offsetTop + headline.offsetHeight * 40;
+    const isHalfShown = showHeadlineAt > headline.offsetTop;
+    const isNotScrolledPast = window.scrollY < headlineBottom;
+    if(isHalfShown && isNotScrolledPast) {
+      headline.classList.add('headline-show');
+    } else {
+      headline.classList.remove('headline-show');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(showContent));
+
+window.addEventListener('scroll', debounce(showHeadline), {capture: true});
 
 
 
